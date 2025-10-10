07ada7be529f56b82b37583a2ae891009dbd22e3
Jin-Security <jjpark@aimedic.kr>
2025-10-10 21:12:53 +0900

    과연

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,8 +1,5 @@
 import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
-  apiBaseUrl:
-    process.env.NEXT_PHASE === 'phase-production-build'
-      ? 'https://www.notion.so/api/v3'
-      : 'https://1wi11.notion.site/api/v3'
+  apiBaseUrl: process.env.NOTION_API_BASE_URL
 })
